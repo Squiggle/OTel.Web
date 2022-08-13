b516f9788a8c7a5388aea0f6663f2d3f25a83a63
Jonathan Gilroy-Powell <jonathan.gilroy-powell@and.digital>
2022-08-13 16:29:41 +0100

    Observability honeycomb example

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Nodes;
+using Honeycomb.OpenTelemetry;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHoneycomb(builder.Configuration);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/wikipedia/{name}", async (string name) => await Lookup(name));
+
+app.Run();
+
+static async Task<string> Lookup(string name)
+{
+  using (HttpClient client = new HttpClient())
+  {
+    client.DefaultRequestHeaders.Add("User-Agent", "Learning-Demo-Client");
+    var response = await client.GetStringAsync($"https://en.wikipedia.org/api/rest_v1/page/summary/{name}?redirect=false");
+
+    using (var span = TracerProvider.Default.GetTracer("my-app").StartActiveSpan("parse-response"))
+    {
+      span.SetAttribute("wikipedialookup.name", name);
+      return JsonNode.Parse(response)?["extract"]?.GetValue<string>() ?? "";
+    }
+  }
+}
